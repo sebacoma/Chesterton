38ee2d6841d76b1cb6bd78924bd885670d924814
sebacoma <sebacoma@gmail.com>
2024-08-08 12:27:51 -0400

    fix: name of sucursales

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -73,9 +73,9 @@ export class HomeComponent implements OnInit, OnDestroy {
   industryAutoplayInterval: any;
   numVisibleSlides = 3;
   branches = [
-    { title: 'Sucursal 1', address: 'Dirección 1', phone: 'Teléfono 1', email: 'Correo 1', image: 'assets/sucursal1.png' },
-    { title: 'Sucursal 2', address: 'Dirección 2', phone: 'Teléfono 2', email: 'Correo 2', image: 'assets/sucursal2.png' },
-    { title: 'Sucursal 3', address: 'Dirección 3', phone: 'Teléfono 3', email: 'Correo 3', image: 'assets/sucursal3.png' }
+    { title: 'Oficina Santiago', address: 'Avenida Los Vientos #20090, Bodega B, Pudahuel, Santiago', phone: ' (2) 29 44 4631', email: 'chile2@chesterton.com', image: 'assets/sucursal1.png' },
+    { title: 'Oficina Concepción', address: 'Camino a Coronel Km.10, #5580, Módulo 7-A Megacentro, San Pedro de la Paz, Concepción', phone: ' (41) 246 0300', email: ' chile1@chesterton.com', image: 'assets/sucursal2.png' },
+    { title: 'Oficina Antofagasta', address: 'Avda. Edmundo Perez Zujovic, #5554, Antofagasta', phone: ' (55) 255 2090', email: ' chile3@chesterton.com', image: 'assets/sucursal3.png' }
   ];
 
   constructor(private titleService: Title, private router: Router) {
