3be4073a9078e0a4b03843bbe7d19df8445e9ceb
sebacoma <sebacoma@gmail.com>
2024-09-30 17:53:52 -0300

    refactor: update product information button in limpiadores component

--- src/app/pages/productos/hidraulicos/limpiadores/limpiadores.component.ts
+++ src/app/pages/productos/hidraulicos/limpiadores/limpiadores.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-limpiadores',
   templateUrl: './limpiadores.component.html',
   styleUrls: ['./limpiadores.component.css']
 })
 export class LimpiadoresComponent {
-  downloadPDF() {
-    const link = document.createElement('a');
-    link.href = '../../pdfs/Limpiadores_5K_21K.pdf';  // Ruta al archivo PDF en la carpeta de assets
-    link.download = 'Limpiadores_5K_21K.pdf';  // Nombre del archivo cuando se descargue
-    link.click();  // Simular clic en el enlace para descargar
-  }
+
 }
