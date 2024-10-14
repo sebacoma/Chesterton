4e315545f328a21fceff52b5879f603e4a1e1173
sebacoma <sebacoma@gmail.com>
2024-10-14 14:16:09 -0300

    cambios segun reunion

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'; // Importar HttpClient
+import { catchError } from 'rxjs/operators'; // Para manejar errores
+import { of } from 'rxjs'; // Para devolver un observable vacío en caso de error
 
 @Component({
   selector: 'app-contacto',
@@ -9,7 +12,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ContactoComponent {
   contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       lastname: ['', [Validators.required, Validators.minLength(3)]],
@@ -22,7 +25,23 @@ export class ContactoComponent {
   onSubmit() {
     if (this.contactForm.valid) {
       // Aquí puedes manejar el envío del formulario
-      console.log(this.contactForm.value);
+      const formData = this.contactForm.value;
+
+      this.http.post('http://localhost:3000/api/formulario', formData)
+        .pipe(
+          catchError(error => {
+            console.error('Error al enviar el formulario:', error);
+            alert('Hubo un problema al enviar tu consulta.'+ error.message); // Mensaje de error al usuario
+            return of(null); // Devuelve un observable vacío
+          })
+        )
+        .subscribe(response => {
+          if (response) {
+            console.log('Respuesta del servidor:', response);
+            alert('Consulta enviada correctamente.'); // Mensaje de éxito
+            this.contactForm.reset(); // Limpiar el formulario
+          }
+        });
     } else {
       // Puedes manejar los errores aquí si lo deseas
       console.log('El formulario contiene errores');
