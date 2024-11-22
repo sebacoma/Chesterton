f858e0db5fa0a95c9448437cc0310d29fc6caff5
sebacoma <sebacoma@gmail.com>
2024-11-22 12:15:26 -0300

    Add EmailJS integration for contact form submission and update styles in historia component

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http'; // Importar HttpClient
-import { catchError } from 'rxjs/operators'; // Para manejar errores
-import { of } from 'rxjs'; // Para devolver un observable vacío en caso de error
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contacto',
@@ -12,7 +10,7 @@ import { of } from 'rxjs'; // Para devolver un observable vacío en caso de erro
 export class ContactoComponent {
   contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       lastname: ['', [Validators.required, Validators.minLength(3)]],
@@ -24,29 +22,28 @@ export class ContactoComponent {
 
   onSubmit() {
     if (this.contactForm.valid) {
-      // Aquí puedes manejar el envío del formulario
       const formData = this.contactForm.value;
 
-      this.http.post('http://localhost:3000/api/formulario', formData)
-        .pipe(
-          catchError(error => {
-            console.error('Error al enviar el formulario:', error);
-            alert('Hubo un problema al enviar tu consulta.'+ error.message); // Mensaje de error al usuario
-            return of(null); // Devuelve un observable vacío
-          })
+      // Envía el formulario usando EmailJS
+      emailjs
+        .send(
+          'service_p96jn2s', // Reemplaza con tu Service ID
+          'contacto', // Reemplaza con tu Template ID
+          formData,
+          'xAZsWmskyb37Mdp0a' // Reemplaza con tu Public Key
         )
-        .subscribe(response => {
-          if (response) {
-            console.log('Respuesta del servidor:', response);
-            alert('Consulta enviada correctamente.'); // Mensaje de éxito
-            this.contactForm.reset(); // Limpiar el formulario
+        .then(
+          response => {
+            console.log('Correo enviado con éxito!', response.status, response.text);
+            alert('Consulta enviada correctamente, pronto nos comunicaremos con usted.');
+            this.contactForm.reset();
+          },
+          error => {
+            console.error('Error al enviar el correo:', error);
+            alert('Hubo un problema al enviar tu consulta.');
           }
-        });
+        );
     } else {
-      // Puedes manejar los errores aquí si lo deseas
-      console.log('El formulario contiene errores');
-      
-      // Marca todos los campos como tocados para mostrar los mensajes de error
       this.contactForm.markAllAsTouched();
     }
   }
