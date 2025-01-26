d2251855cb1a9030a5fd92b0e755ed93b55f7c67
sebacoma <sebacoma@gmail.com>
2025-01-26 14:39:29 -0300

    fix: update titles to include "Mecánicos" for seal components and improve descriptions

--- src/app/pages/productos/sellos-mecanicos/sellos-mecanicos.component.ts
+++ src/app/pages/productos/sellos-mecanicos/sellos-mecanicos.component.ts
@@ -7,15 +7,15 @@ import { Component } from '@angular/core';
 })
 export class SellosMecanicosComponent {
   seals = [
-    { title: 'Sellos Partidos', link: 'sellos-partidos' },
-    { title: 'Sellos de Cartucho', link: 'sellos-cartucho' },
-    { title: 'Sellos de Cassette', link: 'sellos-casete' },
-    { title: 'Sellos de Gas', link: 'sellos-gas' },
-    { title: 'Sellos de Fuelle', link: 'sellos-fuelles' },
-    { title: 'Sellos para Lodo', link: 'sellos-lodos' },
-    { title: 'Sellos para Mezcladores/Agitadores', link: 'sellos-mezcla-agitadores' },
-    { title: 'Sellos de Componentes', link: 'sellos-componentes' },
-    { title: 'Sellos Especializados', link: 'sellos-especializados' },
-    { title: 'Sistemas de Soporte de Sellado', link: 'sellos-soporte-sellado' }
+    { title: 'Sellos Mecánicos Partidos', link: 'sellos-partidos' },
+    { title: 'Sellos Mecánicos de Cartucho', link: 'sellos-cartucho' },
+    { title: 'Sellos Mecánicos de Cassette', link: 'sellos-casete' },
+    { title: 'Sellos Mecánicos de Gas', link: 'sellos-gas' },
+    { title: 'Sellos Mecánicos de Fuelle', link: 'sellos-fuelles' },
+    { title: 'Sellos Mecánicos para Lodo', link: 'sellos-lodos' },
+    { title: 'Sellos Mecánicos para Mezcladores/Agitadores', link: 'sellos-mezcla-agitadores' },
+    { title: 'Sellos Mecánicos de Componentes', link: 'sellos-componentes' },
+    { title: 'Sellos Mecánicos Especializados', link: 'sellos-especializados' },
+    { title: 'Sistema de Soporte de Sellado para Sellos Mecánicos (Plan Ambiental API)', link: 'sellos-soporte-sellado' }
   ];
 }
