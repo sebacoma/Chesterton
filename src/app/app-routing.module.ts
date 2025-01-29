7d3fbce2d50a999e562f4785e8603cd3eb1f579b
sebacoma <sebacoma@gmail.com>
2025-01-29 13:31:27 -0300

    fix: enhance header navigation with dropdown for services and enable scroll position restoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -80,7 +80,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
