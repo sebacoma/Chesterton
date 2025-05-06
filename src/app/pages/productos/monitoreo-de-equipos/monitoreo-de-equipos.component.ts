a64b8d58561319a98c660563909071be72027e37
sebacoma <sebacomaia@gmail.com>
2025-05-05 21:38:17 -0400

    refactor: update navigation links and enhance Chesterton Connect pump configuration page with new content and styles

--- src/app/pages/productos/monitoreo-de-equipos/monitoreo-de-equipos.component.ts
+++ src/app/pages/productos/monitoreo-de-equipos/monitoreo-de-equipos.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class MonitoreoDeEquiposComponent {
   seals = [
     { title: 'Sistema Chesterton Connect™', link: 'sistema-chesterton-connect' },
-    { title: 'Chesterton IIOT Home™', link: 'chesterton-iiot' },
     { title: 'Chesterton Connect™ - Configuración de Bomba', link: 'chesterton-connect-pump-configuration' },
     { title: 'Chesterton Connect™ - Sensores', link: 'chesterton-connect-sensors' },
     { title: 'Chesterton Connect™ - Accesibilidad Remota', link: 'chesterton-connect-remote-accessibility' },
